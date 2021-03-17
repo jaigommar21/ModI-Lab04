6372a6c05b27c714a7a9640250f427c92a962866
usuario01 <usuario01@gmail.com>
2021-03-16 21:24:11 -0500

    Update

--- src/pe/edu/tecsup/collections/map/TestMain7.java
+++ src/pe/edu/tecsup/collections/map/TestMain7.java
@@ -12,8 +12,7 @@ public class TestMain7 {
 
 	public static void main(String[] args) {
 		
-		Map<Integer, Persona> personas 
-				= new HashMap<Integer, Persona>();
+		Map<Integer, Persona> personas = new HashMap<Integer, Persona>();
 		
 		Persona p1 = new Persona("73456582", "Juan Velardes");
 		Persona p2 = new Persona("45238754", "Miguel Chavez");
@@ -22,10 +21,11 @@ public class TestMain7 {
 		personas.put(1001,p1);
 		personas.put(1002,p2);
 		personas.put(1003,p3);
+		personas.put(1004,p3);
 		
-		System.out.println(personas.get(1001));		
-				
-		System.out.println(personas.keySet());		
+		System.out.println(personas.get(1003));		
+
+		System.out.println(personas.keySet());
 		
 		System.out.println(personas.values());
 		
@@ -33,7 +33,6 @@ public class TestMain7 {
 		for (Persona p : c) {
 			System.out.println(p);
 		}
-		
 		
 	}
 
