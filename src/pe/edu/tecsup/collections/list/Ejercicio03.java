8138d36b7721a85ca941b4fc67d1722c740925a2
usuario01 <usuario01@gmail.com>
2021-03-16 19:56:43 -0500

    Update

---
+++ src/pe/edu/tecsup/collections/list/Ejercicio03.java
@@ -0,0 +1,24 @@
+package pe.edu.tecsup.collections.list;
+
+public class Ejercicio03 {
+
+	/*
+	Crear un arreglo de tipo List, que tenga lo siguiente
+
+	 T , E, C , S , U , P 
+
+	Encontrar la posicion de la S, 
+	Reemplazar la letra S por N
+	Reemplazar la letra U por O
+	Remover la letra P
+	Imprimir todo el listado
+
+	*/
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
