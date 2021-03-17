609aba025266c629048315c98c16d99919c30789
usuario01 <usuario01@gmail.com>
2021-03-16 20:04:05 -0500

    Update

--- src/pe/edu/tecsup/collections/list/Ejercicio03.java
+++ src/pe/edu/tecsup/collections/list/Ejercicio03.java
@@ -1,5 +1,8 @@
 package pe.edu.tecsup.collections.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ejercicio03 {
 
 	/*
@@ -15,10 +18,70 @@ public class Ejercicio03 {
 
 	*/
 
+	/*
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		List <String> empresa= new ArrayList<String>();
+		empresa.add("T");
+		empresa.add("E");
+		empresa.add("C");
+		empresa.add("S");
+		empresa.add("U");
+		empresa.add("P");
+		
+		System.out.println(empresa);
+		System.out.println(empresa.indexOf("S"));
+		
+		//empresa.set(3, "N");
+		empresa.set(empresa.indexOf("S"), "N");        
+		System.out.println(empresa);
+		
+		//empresa.set(4, "O");
+        empresa.set(empresa.indexOf("U"), "O");
+		System.out.println(empresa);
+        
+        empresa.remove(empresa.indexOf("P"));
+		System.out.println(empresa);
+        
+        for(String letra: empresa) {
+            System.out.println(letra);
+        }
+		
+		
 	}
 
+	*/
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<String> tecsup = new ArrayList<String>();
+        tecsup.add("T");
+        tecsup.add("E");
+        tecsup.add("C");
+        tecsup.add("S");
+        tecsup.add("U");
+        tecsup.add("P");
+        
+        System.out.println(tecsup);
+        
+        int index = tecsup.indexOf("S");
+        System.out.println("La posicion de la S = " + index);
+        
+        tecsup.set(index, "N");
+        
+        int index1 = tecsup.indexOf("U");
+        tecsup.set(index1, "O");
+        
+        tecsup.remove(5);
+        
+        for (String string : tecsup) {
+            System.out.println("nombre = " + string);
+        }
+		
+	}
+
+	
 }
